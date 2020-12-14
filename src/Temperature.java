ab721c5ae8a138b10141a33ae25556e24af90fad
semen-nenashev <nenashev.sd@gmail.com>
2020-12-14 12:59:55 +0200

    formatting

--- src/Temperature.java
+++ src/Temperature.java
@@ -1,27 +1,26 @@
 public class Temperature {
- public static double fahrenheit;
- public static double celsius;
-  /**
-   * Converts temperature from Fahrenheit to Celsius degree.
-   *
-   * @param fahrenheit temperature degree in Fahrenheit
-   * @return temperature degree in Celsius
-   */
-  public static double fahrenheitToCelsius(double fahrenheit) {
-    // TODO fill in code here and replace the return statement
-      celsius = 5*(fahrenheit - 32)/9;
-    return celsius;
-  }
+    public static double fahrenheit;
+    public static double celsius;
 
-  /**
-   * Converts temperature from Celsius to Fahrenheit degree.
-   *
-   * @param celsius temperature degree in Celsius
-   * @return temperature degree in Fahrenheit
-   */
-  public static double celsiusToFahrenheit(double celsius) {
-    // TODO fill in code here and replace the return statement
-      fahrenheit = (9*celsius + 160)/5;
-    return fahrenheit;
-  }
+    /**
+     * Converts temperature from Fahrenheit to Celsius degree.
+     *
+     * @param fahrenheit temperature degree in Fahrenheit
+     * @return temperature degree in Celsius
+     */
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        celsius = 5 * (fahrenheit - 32) / 9;
+        return celsius;
+    }
+
+    /**
+     * Converts temperature from Celsius to Fahrenheit degree.
+     *
+     * @param celsius temperature degree in Celsius
+     * @return temperature degree in Fahrenheit
+     */
+    public static double celsiusToFahrenheit(double celsius) {
+        fahrenheit = (9 * celsius + 160) / 5;
+        return fahrenheit;
+    }
 }
