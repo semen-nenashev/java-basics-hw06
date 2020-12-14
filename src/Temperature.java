ebcad0ab5105880800bc6687a4319e480cc0e239
semen-nenashev <nenashev.sd@gmail.com>
2020-12-14 12:58:26 +0200

    homework 6 done

--- src/Temperature.java
+++ src/Temperature.java
@@ -1,5 +1,6 @@
 public class Temperature {
-
+ public static double fahrenheit;
+ public static double celsius;
   /**
    * Converts temperature from Fahrenheit to Celsius degree.
    *
@@ -8,7 +9,8 @@ public class Temperature {
    */
   public static double fahrenheitToCelsius(double fahrenheit) {
     // TODO fill in code here and replace the return statement
-    return 0;
+      celsius = 5*(fahrenheit - 32)/9;
+    return celsius;
   }
 
   /**
@@ -19,6 +21,7 @@ public class Temperature {
    */
   public static double celsiusToFahrenheit(double celsius) {
     // TODO fill in code here and replace the return statement
-    return 0;
+      fahrenheit = (9*celsius + 160)/5;
+    return fahrenheit;
   }
 }
